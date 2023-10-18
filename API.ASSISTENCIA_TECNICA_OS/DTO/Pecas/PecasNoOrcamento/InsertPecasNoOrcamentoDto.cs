fdde614795f10780e9cc1ad01bfff516b98ed7a3
vihuro <vitorhugorodrigues93@gmail.com>
2023-10-18 17:57:56 -0300

    ultiumos códigos

---
+++ API.ASSISTENCIA_TECNICA_OS/DTO/Pecas/PecasNoOrcamento/InsertPecasNoOrcamentoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace API.ASSISTENCIA_TECNICA_OS.DTO.Pecas.PecasNoOrcamento
+{
+    public class InsertPecasNoOrcamentoDto
+    {
+        public int NumeroOcamento { get; set; }
+        public Guid UsuarioId { get; set; }
+        public List<PecasResumidaOrcamentoDto> Pecas { get; set; }
+    }
+
+    public class PecasResumidaOrcamentoDto
+    {
+        public Guid PecaId { get; set; }
+    }
+}
---
+++ API.ASSISTENCIA_TECNICA_OS/DTO/Pecas/PecasNoOrcamento/ReturnPecasOrcamentoDto.cs
@@ -0,0 +1,6 @@
+﻿namespace API.ASSISTENCIA_TECNICA_OS.DTO.Pecas.PecasNoOrcamento
+{
+    public class ReturnPecasOrcamentoDto
+    {
+    }
+}
